b2fc8b3a9d7d4ec9a61f6a2d4c13956edd046dd7
shailjadosar12 <shailjadosar12@gmail.com>
2024-02-12 09:10:31 +0530

    background color changer project

--- 03tailwindsprops/src/App.jsx
+++ 03tailwindsprops/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="bg-green-400">Tailwind test</h1>
+      <div className="m-3">
+        <Card username="kanak" btntxt="about me"></Card>
+        <Card username="shailja" btntxt="view me"></Card>
+        <Card username="shrishti" btntxt="view profile"></Card>
+      </div>
     </>
   );
 }
